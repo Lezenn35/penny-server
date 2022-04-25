b89a9d0fad93065f58d75e60b462cda8deae2b3c
Lezenn <gireg.lezoraine2022@campus-eni.fr>
2022-04-26 00:44:04 +0200

    Added fastify sockets

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
+import { SocketStream } from "fastify-websocket";
 
 const server = fastify();
+server.register(require('fastify-websocket'));
+
+server.get("/game", {websocket: true}, (connection: SocketStream, req: FastifyRequest) => {
+    connection.socket.on('message', message => {
+        connection.socket.send('hi from server');
+    })
+});
 
 server.get('/ping', async (request, reply) => {
     return 'pong\n';
